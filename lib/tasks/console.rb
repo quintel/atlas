df93600d2477bf5414704cc3a6229859175b42ca
Anthony Williams <hi@antw.me>
2013-05-28 15:31:49 +0100

    rake console accepts a path to the ETSource dir.

--- lib/tasks/console.rb
+++ lib/tasks/console.rb
@@ -1,9 +1,12 @@
 namespace :console do
-  task :run do
+  task :run, [:dir] do |_, args|
     command = system("which pry > /dev/null 2>&1") ? 'pry' : 'irb'
-    exec "#{ command } -I./lib -r./lib/etsource.rb"
+    dir     = args.dir.nil? ? '../etsource/data' : args.dir
+    post    = %('ETSource.data_dir = #{ dir.inspect }')
+
+    exec "#{ command } -I./lib -r./lib/etsource.rb -e #{ post }"
   end
 end
 
 desc 'Open a pry or irb session preloaded with Turbine'
-task console: ['console:run']
+task :console, [:dir] => ['console:run']
