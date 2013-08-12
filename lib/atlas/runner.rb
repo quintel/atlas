e2acc121919a4af5cd5d6d484a9a7f61af720876
Anthony Williams <hi@antw.me>
2013-08-12 14:07:24 +0100

    Ignore coupling carriers when setting slot shares

--- lib/atlas/runner.rb
+++ lib/atlas/runner.rb
@@ -17,6 +17,8 @@ module Atlas
           (model.out_slots + model.in_slots).each do |slot|
             collection = node.slots.public_send(slot.direction)
 
+            next if slot.carrier == :coupling_carrier
+
             if collection.include?(slot.carrier)
               ref_slot = collection.get(slot.carrier)
             else
@@ -24,11 +26,11 @@ module Atlas
             end
 
             ref_slot.set(:model, slot)
-            ref_slot.set(:type, :elastic) if ref_slot.is_a?(Slot::Elastic)
+            ref_slot.set(:type, :elastic) if slot.is_a?(Slot::Elastic)
 
             if slot.query
               ref_slot.set(:share, query.call(slot.query))
-            else
+            elsif slot.share
               ref_slot.set(:share, slot.share)
             end
           end
