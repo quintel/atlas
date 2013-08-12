1ce3142423c0dbf1c30fd000b1a62912167e5767
Anthony Williams <hi@antw.me>
2013-08-12 14:07:43 +0100

    Add console:refinery task
    Gives you the graph with all queries set, but without yet having been
    calculated by Refinery.

--- lib/tasks/console.rb
+++ lib/tasks/console.rb
@@ -1,10 +1,25 @@
 namespace :console do
-  task :run, [:dir] do |_, args|
+  def start_console(dir, prelude = nil)
     command = system("which pry > /dev/null 2>&1") ? 'pry' : 'irb'
-    dir     = args.dir.nil? ? '../etsource/data' : args.dir
-    post    = %('Atlas.data_dir = #{ dir.inspect }')
+    dir     = dir.nil? ? '../etsource/data' : dir
+    prelude = %(Atlas.data_dir = #{ dir.inspect } ; #{ prelude })
 
-    exec "#{ command } -I./lib -r./lib/atlas.rb -e #{ post }"
+    prelude.gsub!("\n", '; ')
+
+    exec "#{ command } -I./lib -r./lib/atlas.rb -e '#{ prelude }'"
+  end
+
+  task :run, [:dir] do |_, args|
+    start_console(args.dir)
+  end
+
+  desc 'Opens a console with a graph ready for calculation by Refinery'
+  task :refinery, [:dir] do |_, args|
+    start_console(args.dir, <<-RUBY)
+      turbine = Atlas::GraphBuilder.build
+      runner  = Atlas::Runner.new(Atlas::Dataset.find(:nl), turbine)
+      graph   = runner.refinery_graph
+    RUBY
   end
 end
 
