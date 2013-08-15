b2e79f3424be1d62488eddce1cf3bb719daf336c
Anthony Williams <hi@antw.me>
2013-08-15 14:25:07 +0100

    ProductionMode is now created with a hash
    This is instead of referring to some pre-defined location in ETSource,
    so that we can more easily test production mode objects in ETEngine.
    But, more importantly, allows us to "lazy-compile" the graph when needed
    in ETEngine, and save the static file to its ./tmp direction instead of
    it having to exist in ETSource.

--- spec/atlas/production_mode_spec.rb
+++ spec/atlas/production_mode_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 module Atlas
   describe ProductionMode, :fixtures do
-    let(:mode) { ProductionMode.new(:nl) }
+    let(:mode) { ProductionMode.new({ nodes: { foo: { demand: 50 } } }) }
 
     it 'retrieves nodes' do
       expect(mode.nodes.length).to eq(Atlas::Node.all.length)
     end
 
     it 'uses static data' do
-      expect(mode.nodes.find(:foo).demand).to eq(50)
+      expect(mode.nodes.find(:foo).demand).to eql(50.0)
     end
 
     it 'retrieves edges' do
