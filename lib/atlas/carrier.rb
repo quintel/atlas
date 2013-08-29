b2360c928973c30756cfc14d2a7f29d65bca4c96
Anthony Williams <hi@antw.me>
2013-08-29 15:00:40 +0100

    Add FCE CO2 attributes to Carrier

--- lib/atlas/carrier.rb
+++ lib/atlas/carrier.rb
@@ -13,6 +13,13 @@ module Atlas
     attribute :kg_per_liter,               Float
     attribute :graphviz_color,             Symbol
 
+    attribute :co2_conversion_per_mj,      Float
+    attribute :co2_exploration_per_mj,     Float
+    attribute :co2_extraction_per_mj,      Float
+    attribute :co2_treatment_per_mj,       Float
+    attribute :co2_transportation_per_mj,  Float
+    attribute :co2_waste_treatment_per_mj, Float
+
     attribute :fce,                        Hash[Symbol => Hash]
 
   end # Carrier
