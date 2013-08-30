942073ba0a0574996dc92d5a2f23e6553d37316f
Anthony Williams <hi@antw.me>
2013-08-30 16:00:32 +0100

    Update exporter specs to set documents on Refinery objects

--- spec/atlas/exporter_spec.rb
+++ spec/atlas/exporter_spec.rb
@@ -4,6 +4,16 @@ module Atlas
   describe Exporter, :fixtures do
     before { Atlas.load_library('refinery') }
 
+    def make_node(key, attributes = {})
+      model = Atlas::Node.new(key: key)
+      Refinery::Node.new(key, attributes.merge(model: model))
+    end
+
+    def make_edge(from, to, carrier, attributes = {})
+      model = Atlas::Edge.new(key: Atlas::Edge.key(to.key, from.key, carrier))
+      from.connect_to(to, carrier, attributes.merge(model: model))
+    end
+
     #  (25) [M] -- 10 --> [F] (10)
     #         \           /
     #          \         /
@@ -15,14 +25,14 @@ module Atlas
     let!(:graph)   { Turbine::Graph.new }
 
     # Nodes
-    let!(:mother)  { graph.add Refinery::Node.new(:mother, demand: 25) }
-    let!(:father)  { graph.add Refinery::Node.new(:father, demand: 10) }
-    let!(:child)   { graph.add Refinery::Node.new(:child,  demand: 25) }
+    let!(:mother)  { graph.add(make_node(:mother, demand: 25)) }
+    let!(:father)  { graph.add(make_node(:father, demand: 10)) }
+    let!(:child)   { graph.add(make_node(:child,  demand: 25)) }
 
     # Edges
-    let!(:mf_edge) { mother.connect_to(father, :spouse, child_share: 1.0) }
-    let!(:mc_edge) { mother.connect_to(child,  :child,  child_share: (15.0 / 25.0)) }
-    let!(:fc_edge) { father.connect_to(child,  :child,  child_share: (10.0 / 25.0)) }
+    let!(:mf_edge) { make_edge(mother, father, :spouse, child_share: 1.0) }
+    let!(:mc_edge) { make_edge(mother, child,  :child,  child_share: (15.0 / 25.0)) }
+    let!(:fc_edge) { make_edge(father, child,  :child,  child_share: (10.0 / 25.0)) }
 
     # Slots
     let!(:mf_slot) { mother.slots.out(:spouse).set(:share, 10.0 / 25.0) }
