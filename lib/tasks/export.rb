9bbc6dd6dbc390708186cf760f7911ecfbea4484
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-09-11 10:20:15 +0200

    Creating tmp/export/nodes directory
    And creating it if it does not exist yet.

--- lib/tasks/export.rb
+++ lib/tasks/export.rb
@@ -4,15 +4,22 @@ namespace :export do
   task :nodes do
     require 'atlas'
     require 'csv'
+    require 'fileutils'
 
     Atlas.data_dir = '../etsource/data'
 
-    Dir.chdir('tmp')
+    export_dir = 'tmp/export/nodes'
+
+    FileUtils.mkdir_p(export_dir)
+
+    Dir.chdir(export_dir)
 
     Atlas::Node.all.each do |node|
-      File.open(node.key, 'w') do
-        node.to_csv
+      file_name = "#{ node.key.to_s }.csv"
+      File.open(file_name, 'w') do |f|
+        f.write(node.to_csv)
       end
+      puts "* #{ file_name } created in #{ export_dir }"
     end
 
   end
