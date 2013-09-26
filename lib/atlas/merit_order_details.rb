ac7a269f311752512b2afcc0e0d023ebd11cb84c
Anthony Williams <hi@antw.me>
2013-09-26 11:33:02 +0100

    HashToText can cope with embedded objects
    An embedded object is now converted into a hash before saving to the
    ActiveDocument file.

--- lib/atlas/merit_order_details.rb
+++ lib/atlas/merit_order_details.rb
@@ -4,5 +4,16 @@ module Atlas
 
     attribute :type,  Symbol
     attribute :group, Symbol
+
+    # Public: The merit order details as a hash, with any nil attributes not
+    # present.
+    #
+    # Returns a hash.
+    def to_hash
+      attrs = attributes
+      attrs.delete_if { |_, value| value.nil? }
+
+      attrs
+    end
   end # MeritOrderDetails
 end # Atlas
