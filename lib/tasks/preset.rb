215735f366c53dda18b2201ff2b74278ea1e2935
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2013-11-07 15:47:02 +0100

    Dealing with input_values that do not 'exist' anymore.
    Also increased the verbosity of the rake task

--- lib/tasks/preset.rb
+++ lib/tasks/preset.rb
@@ -23,17 +23,38 @@ namespace :preset do
 
     Atlas.data_dir = '../etsource/data'
 
-    data = retrieve_preset_data!(ENV['SERVER'], ENV['ID']).with_indifferent_access
+    server = ENV['server'] || 'et-engine.com'
+    id     = ENV['ID']     || 'please provide which (scenario) ID!'
+
+    puts "Retrieving data from #{ server } for scenario_id=#{ id }"
+
+    data = retrieve_preset_data!(server, id).with_indifferent_access
 
     data[:key] = ENV['KEY'] || data[:id].to_s
 
+    data[:user_values] = prune_user_values(data[:user_values])
+
     preset = Atlas::Preset.new(data)
     preset.save!
+
+    puts "Scenario saved in #{ preset.path }"
+    puts "Done!"
   end
 
   def retrieve_preset_data!(server_path, scenario_id)
     url = "http://#{ server_path }/api/v3/scenarios/#{ scenario_id }?detailed=true"
     HTTParty.get(url)
+  end
+
+  def prune_user_values(user_values)
+    user_values.inject({}) do |memory, (key, value)|
+      if Atlas::Input.manager.key?(key)
+        memory[key] = value
+      else
+        puts "WARNING: #{ key } not found in Inputs and has been DROPPED!!!"
+      end
+      memory
+    end
   end
 
 end
