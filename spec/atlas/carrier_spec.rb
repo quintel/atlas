fb06006f82d1003815412ca80958612d291bbf2a
Anthony Williams <hi@antw.me>
2014-02-06 17:53:02 +0000

    Add support for regional FCE carrier data
    Ref quintel/etsource#708

--- spec/atlas/carrier_spec.rb
+++ spec/atlas/carrier_spec.rb
@@ -8,6 +8,20 @@ module Atlas
       expect(Carrier.all).to have_at_least(1).item
     end
 
+    describe '#fce' do
+      it 'loads FCE data when present' do
+        expect(Carrier.find(:coal).fce(:nl)).to_not be_empty
+      end
+
+      it 'does not load FCE data for a region with no data' do
+        expect(Carrier.find(:coal).fce(:uk)).to_not be
+      end
+
+      it 'does not load FCE data for a carrier with no data' do
+        expect(Carrier.find(:corn).fce(:nl)).to_not be
+      end
+    end # fce
+
   end
 
 end
