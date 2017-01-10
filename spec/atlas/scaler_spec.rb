bdf08b41d2ca2059498e3cf0edac8220b4316893
Peter Lohmann <peter.lohmann@quintel.com>
2017-01-10 11:56:26 +0100

    Make specs for GraphScaler independent of graph importing

--- spec/atlas/scaler_spec.rb
+++ spec/atlas/scaler_spec.rb
@@ -1,29 +1,29 @@
 require 'spec_helper'
 
 module Atlas; describe Scaler do
+  include GraphHelper
+
+  # Graph:
+  # [ a (25) ]--< a_b (10) >--[ b (10) ]
+
+  let(:graph) do
+    Turbine::Graph.new.tap do |graph|
+      a = graph.add(make_node(:a, demand: 25))
+      b = graph.add(make_node(:b, demand: 10))
+      make_edge(a, b, :a_b, child_share: 1.0, demand: 10)
+    end
+  end
+
+  let(:scaling_factor) { 1000.to_r / 7_349_500 }
+
+  before { allow(GraphBuilder).to receive(:build).and_return(graph) }
+
+
   describe '#create_scaled_dataset' do
-    include GraphHelper
-
-    # Graph:
-    # [ a (25) ]--< a_b (10) >--[ b (10) ]
-
-    let(:graph) { Turbine::Graph.new }
-
-    let!(:a)  { graph.add(make_node(:a, demand: 25)) }
-    let!(:b)  { graph.add(make_node(:b, demand: 10)) }
-
-    let!(:ab_edge) { make_edge(a, b, :a_b, child_share: 1.0, demand: 10) }
-
-
-    let!(:mock_graph) {
-      allow(GraphBuilder).to receive(:build).and_return(graph)
-    }
-
     let(:derived_dataset) { Atlas::Dataset::DerivedDataset.find('ameland') }
 
     context 'with scaling value 1000' do
-      let(:scaler) { Atlas::Scaler.new('nl', 'ameland', 1000) }
-      let(:scaling_divisor) { (7_349_500.to_f / 1000).to_r }
+      let(:scaler) { Scaler.new('nl', 'ameland', 1000) }
 
       before { scaler.create_scaled_dataset }
 
@@ -63,25 +63,32 @@ module Atlas; describe Scaler do
       it 'dumps a graph.yml' do
         expect(derived_dataset.graph).to_not be_blank
       end
-
-      it 'exports the correct demand 25/scaling for node :a' do
-        expect(derived_dataset.graph.node(:a).get(:demand)).
-          to eql(25.to_r / scaling_divisor)
-      end
-
-      it 'exports the correct demand 10/scaling for edge :a->:b' do
-        expect(derived_dataset.graph.node(:a).out_edges.first.get(:demand)).
-          to eql(10.to_r / scaling_divisor)
-      end
     end
 
     context 'with scaling value nil' do
-      let(:scaler) { Atlas::Scaler.new('nl', 'ameland', nil) }
+      let(:scaler) { Scaler.new('nl', 'ameland', nil) }
 
       it 'creates an invalid DerivedDataset' do
         expect { scaler.create_scaled_dataset }.
           to raise_error(Atlas::InvalidDocumentError, /Scaling Value/)
       end
     end
-  end
-end; end
+  end # create_scaled_dataset
+
+
+  describe Scaler::GraphScaler do
+    let(:graph_data) { EssentialExporter.dump(graph) }
+
+    before { Scaler::GraphScaler.new(scaling_factor).call(graph_data) }
+
+    it 'exports the correct demand 25 * scaling_factor for node :a' do
+      expect(graph_data[:nodes][:a][:demand]).
+        to eql(25.to_r * scaling_factor)
+    end
+
+    it 'exports the correct demand 10 * scaling_factor for edge :a->:b' do
+      expect(graph_data[:edges][:'a-b@a_b'][:demand]).
+        to eql(10.to_r * scaling_factor)
+    end
+  end # GraphScaler
+end; end # Atlas::Scaler
