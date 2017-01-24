c5f17e60a5652a57755874c581008554092adcb0
Gerard Westerhof <gerard@grdw.nl>
2017-01-24 16:59:33 +0100

    Add 'update type' property to initializer input class

--- lib/atlas/initializer_input.rb
+++ lib/atlas/initializer_input.rb
@@ -8,6 +8,7 @@ module Atlas
     attribute :share_group,     Symbol
     attribute :priority,        Integer, default: 0
     attribute :unit,            String
+    attribute :update_type,     String
 
     validates_presence_of :query
   end
