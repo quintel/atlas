76d29ba354bac5f44c65dae1236d185c7793ef7c
Gerard Westerhof <gerard@grdw.nl>
2017-01-25 09:29:22 +0100

    Remove unit attribute from the initializer input

--- lib/atlas/initializer_input.rb
+++ lib/atlas/initializer_input.rb
@@ -7,7 +7,6 @@ module Atlas
     attribute :query,           String
     attribute :share_group,     Symbol
     attribute :priority,        Integer, default: 0
-    attribute :unit,            String
     attribute :update_type,     String
 
     validates_presence_of :query
