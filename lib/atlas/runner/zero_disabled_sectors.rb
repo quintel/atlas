883141d2bf151747aea6e043bc34be8a1d3bc6c3
Peter Lohmann <peter.lohmann@quintel.com>
2017-02-04 10:14:33 +0100

    Disable nodes also based on sector

--- lib/atlas/runner/zero_disabled_sectors.rb
+++ lib/atlas/runner/zero_disabled_sectors.rb
@@ -7,21 +7,33 @@ module Atlas
     module ZeroDisabledSectors
       # Maps boolean flags on the dataset to node groups which should be
       # disabled if the flag is false.
-      FEATURE_MAP = { has_metal: :metal_industry }
+      FEATURE_GROUPS = { has_metal: :metal_industry }
+      FEATURE_SECTORS = { has_industry:    :industry,
+                          has_agriculture: :agriculture }
+
 
       # Public: Creates a proc which can be used by Refinery to zero-out any
       # disabled subsectors.
       def self.with_dataset(dataset)
         # Create an array of namespaces which have been disabled in the
         # given dataset.
-        disabled_groups = FEATURE_MAP.reject do |ds_attribute, group|
+        disabled_groups = FEATURE_GROUPS.reject do |ds_attribute, _|
           dataset.send(ds_attribute)
         end.map(&:last)
 
+        disabled_sectors = FEATURE_SECTORS.reject do |ds_attribute, _|
+          dataset.send(ds_attribute)
+        end.map(&:last).map(&:to_s)
+
         lambda do |refinery|
           disabled_nodes = refinery.nodes.select do |node|
-            (node.get(:model).groups & disabled_groups).any?
+            disabled_sectors.include?(node.get(:model).sector.to_s) ||
+              (node.get(:model).groups & disabled_groups).any?
           end
+
+          # @@@ debug
+          p "ZeroDisabledSectors", dataset.key, disabled_groups, disabled_sectors
+          puts disabled_nodes.map(&:key)
 
           disabled_nodes.each(&method(:zero!))
 
