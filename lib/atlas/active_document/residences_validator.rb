dce3f01109e22c720cacdc006ede3408e655e5c6
Gerard Westerhof <gerard@grdw.nl>
2017-10-26 10:43:16 +0200

    Don't validate the residences if the number of new or old residences aren't both present

--- lib/atlas/active_document/residences_validator.rb
+++ lib/atlas/active_document/residences_validator.rb
@@ -2,6 +2,9 @@ module Atlas
   module ActiveDocument
     class ResidencesValidator < ActiveModel::Validator
       def validate(record)
+        return if record.number_of_old_residences.nil? ||
+                  record.number_of_new_residences.nil?
+
         sum_of_residences = record.number_of_old_residences +
                             record.number_of_new_residences
 
