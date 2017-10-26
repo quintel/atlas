d1ea0ba032723a5770947cfc066e43954518dfe2
Gerard Westerhof <gerard@grdw.nl>
2017-10-26 10:36:10 +0200

    Adding a validation for number of residences
    Part of quintel/etmodel#2412

---
+++ lib/atlas/active_document/residences_validator.rb
@@ -0,0 +1,19 @@
+module Atlas
+  module ActiveDocument
+    class ResidencesValidator < ActiveModel::Validator
+      def validate(record)
+        sum_of_residences = record.number_of_old_residences +
+                            record.number_of_new_residences
+
+        unless record.number_of_residences.round == sum_of_residences.round
+          record.errors.add(:number_of_residences,
+            "Number of old residences (#{ record.number_of_old_residences}) "\
+            "and number of new residences (#{ record.number_of_new_residences }) "\
+            "don't add up to the total number of residences "\
+            "(#{ record.number_of_residences })."
+          )
+        end
+      end
+    end
+  end
+end
