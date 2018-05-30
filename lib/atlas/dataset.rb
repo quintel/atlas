562e03b1ffd75f434baa4828e2b0290bcebf7491
Anthony Williams <hi@antw.io>
2018-05-30 16:11:14 +0100

    Add attributes for dynamic solar PV profiles

--- lib/atlas/dataset.rb
+++ lib/atlas/dataset.rb
@@ -116,6 +116,9 @@ module Atlas
     attribute :electric_vehicle_profile_2_share, Float, default: 0.0
     attribute :electric_vehicle_profile_3_share, Float, default: 0.0
 
+    attribute :solar_pv_profile_1_share, Float, default: 1.0
+    attribute :solar_pv_profile_2_share, Float, default: 0.0
+
     # These attributes are relative to the size of the region. If we simulate a
     # sub-region (say, 5% of the "full" region size), these attributes can be
     # reduced in proportion to the sub-region size.
