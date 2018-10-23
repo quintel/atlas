1cc5ae3d5f10ac392ad4e6a9079f3ee7a93e6243
Anthony Williams <hi@antw.io>
2018-10-23 16:06:39 +0100

    Add "subtype" attribute to MeritOrderDetails

--- lib/atlas/merit_order_details.rb
+++ lib/atlas/merit_order_details.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Atlas
   class MeritOrderDetails
     include ValueObject
@@ -5,6 +7,7 @@ module Atlas
 
     values do
       attribute :type,           Symbol
+      attribute :subtype,        Symbol, default: :generic
       attribute :group,          Symbol
       attribute :level,          Symbol, default: :hv
       attribute :delegate,       Symbol
@@ -12,7 +15,17 @@ module Atlas
       attribute :demand_profile, Symbol
     end
 
-    validates :type, inclusion: %i[consumer dispatchable flex must_run volatile]
+    validates :type, inclusion: %i[consumer flex producer]
     validates :level, inclusion: %i[lv mv hv omit]
+
+    # Producer subtypes.
+    validates :subtype,
+      inclusion: %i[dispatchable must_run volatile],
+      if: ->(mod) { mod.type == :producer }
+
+    # Consumer subtypes.
+    validates :subtype,
+      inclusion: %i[generic pseudo],
+      if: ->(mod) { mod.type == :consumer }
   end # MeritOrderDetails
 end # Atlas
