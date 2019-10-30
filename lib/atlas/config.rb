07a2a61ed7568073f437ec2a033961e3b2180c4d
Anthony Williams <hi@antw.dev>
2019-10-30 15:22:47 +0000

    Add Config.read?
    Reads a config file, returning nil if it doesn't exist. Intended only
    for internal use in Atlas.

--- lib/atlas/config.rb
+++ lib/atlas/config.rb
@@ -1,13 +1,37 @@
+# frozen_string_literal: true
+
 module Atlas
+  # Reads YAML config files from the "config" directory.
   module Config
-    # Public: Reads a YAML config file from the ETSource config directory.
-    #
-    # basename - The name of the config file, minus the ".yml" extension.
-    def self.read(basename)
-      basename = basename.to_s.downcase.gsub(/[^a-z0-9_]/, '')
-      YAML.load_file(Atlas.data_dir.join('config').join("#{basename}.yml"))
-    rescue Errno::ENOENT
-      raise DocumentNotFoundError.new(basename, self)
+    class << self
+      # Public: Reads a YAML config file from the ETSource config directory.
+      #
+      # basename - The name of the config file, minus the ".yml" extension.
+      def read(basename)
+        YAML.load_file(path_for_basename(basename))
+      rescue Errno::ENOENT
+        raise DocumentNotFoundError.new(clean_basename(basename), self)
+      end
+
+      # Internal: Reads a YAML config file from the ETSource config directory,
+      # returning nil if it isn't readable.
+      #
+      # See Config.read
+      def read?(basename)
+        if (path = path_for_basename(basename)).file?
+          YAML.load_file(path)
+        end
+      end
+
+      private
+
+      def path_for_basename(basename)
+        Atlas.data_dir.join('config').join("#{clean_basename(basename)}.yml")
+      end
+
+      def clean_basename(basename)
+        basename.to_s.downcase.gsub(/[^a-z0-9_]/, '')
+      end
     end
   end
 end
--- spec/atlas/config_spec.rb
+++ spec/atlas/config_spec.rb
@@ -1,33 +1,55 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-module Atlas
+describe Atlas::Config do
+  shared_examples_for 'successfully reading a config file' do
+    context 'when the file exists' do
+      it 'parses the YAML file content' do
+        expect(described_class.public_send(method_name, 'unscaleable_units')).to eq(%w[a b])
+      end
 
-describe Config do
-  context 'when the file exists' do
-    it 'parses the YAML file content' do
-      expect(Atlas::Config.read('unscaleable_units')).to eq(%w[a b])
+      it 'removes non-alphanumeric characters from the basename' do
+        expect(described_class.public_send(method_name, '../un/sc^aleable_units')).to eq(%w[a b])
+      end
     end
+  end
 
-    it 'removes non-alphanumeric characters from the basename' do
-      expect(Atlas::Config.read('../un/sc^aleable_units')).to eq(%w[a b])
+  describe '.read' do
+    let(:method_name) { :read }
+
+    include_examples 'successfully reading a config file'
+
+    context 'when the file does not exist' do
+      it 'throws an error' do
+        expect { described_class.read('no') }.to raise_error(
+          Atlas::DocumentNotFoundError,
+          'Could not find a config with the key "no"'
+        )
+      end
+
+      it 'removes non-alphanumeric characters from the basename' do
+        expect { described_class.read('no/file') }.to raise_error(
+          Atlas::DocumentNotFoundError,
+          'Could not find a config with the key "nofile"'
+        )
+      end
     end
   end
 
-  context 'when the file does not exist' do
-    it 'throws an error' do
-      expect { Atlas::Config.read('no') }.to raise_error(
-        Atlas::DocumentNotFoundError,
-        'Could not find a config with the key "no"'
-      )
-    end
+  describe '.read?' do
+    let(:method_name) { :read? }
 
-    it 'removes non-alphanumeric characters from the basename' do
-      expect { Atlas::Config.read('no/file') }.to raise_error(
-        Atlas::DocumentNotFoundError,
-        'Could not find a config with the key "nofile"'
-      )
+    include_examples 'successfully reading a config file'
+
+    context 'when the file does not exist' do
+      it 'throws an error' do
+        expect(described_class.read?('no')).to be_nil
+      end
+
+      it 'removes non-alphanumeric characters from the basename' do
+        expect(described_class.read?('no/file')).to be_nil
+      end
     end
   end
-end
-
 end
