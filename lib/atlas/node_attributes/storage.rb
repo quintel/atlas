861bb6310a362e2d1f755f58047af24ad0799e79
Anthony Williams <hi@antw.dev>
2019-12-20 14:13:11 +0000

    Make Storage attribute writers public

--- lib/atlas/node_attributes/storage.rb
+++ lib/atlas/node_attributes/storage.rb
@@ -6,13 +6,13 @@ module Atlas
 
       values do
         # The total amount which may be stored, in Wh.
-        attribute :volume, Float
+        attribute :volume, Float, writer: :public
 
         # The amount by which the stored amount decreases each hour, in W.
-        attribute :decay, Float, default: 0.0
+        attribute :decay, Float, default: 0.0, writer: :public
 
         # Cost per unit of volume installed.
-        attribute :cost_per_mwh, Float, default: 0.0
+        attribute :cost_per_mwh, Float, default: 0.0, writer: :public
       end
     end
   end
