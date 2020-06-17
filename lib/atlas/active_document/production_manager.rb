4c8a8bcb4c099d89ac254777fd448c9a54c61096
Anthony Williams <hi@antw.dev>
2020-06-17 16:43:37 +0100

    Fix ProductionManager having wrong dir or ext
    Fixes that ProductionManager would have an incorrect directory_name or
    extension_name when the class being managed has a custom value for
    either or both.
    
    This would result in the manager finding no objects (on the file
    system) to manage.
    
    `lookup_map` is now filtered to ignore any objects on the file system
    for which data wasn't provided.

--- lib/atlas/active_document/production_manager.rb
+++ lib/atlas/active_document/production_manager.rb
@@ -1,33 +1,51 @@
+# frozen_string_literal: true
+
 module Atlas
   module ActiveDocument
-    # A Manager which loads documents but sets extra attribute values by
-    # loading the exported CSV files. This is used in "production"
-    # environments such as ETEngine when running Rubel queries and Refinery
-    # would be prohibitively slow.
+    # A Manager which loads documents but sets extra attribute values by loading the exported CSV
+    # files. This is used in "production" environments such as ETEngine when running Rubel queries
+    # and Refinery would be prohibitively slow.
     class ProductionManager < Manager
       # Public: Creates a new manager for the given document +klass+.
       #
       # klass - The ActiveDocument class whose files are read by the Manager.
-      #
-      # data  - Production data for the nodes (normally containing demands
-      #         calculated with queries or by Refinery)
+      # data  - Production data for the nodes (normally containing demands calculated with queries
+      #         or by Refinery)
       #
       # Returns a Manager::Production.
       def initialize(klass, data)
         super(klass, false)
+
         @data = data
+        @directory_name = klass.directory_name
+        @extension_name = klass.extension_name
       end
 
       # Production-mode documents may not be changed.
-      %w( delete write delete_path ).each do |method|
+      %w[delete write delete_path].each do |method|
         class_eval <<-RUBY, __FILE__, __LINE__ + 1
-          def #{ method }(*)
+          def #{method}(*)
             raise ReadOnlyError.new
           end
         RUBY
       end
 
       private
+
+      # Internal: A hash connecting the document keys with the path to the file. Omits any documents
+      # which were not specified when creating the ProductionMode.
+      #
+      # Returns an array of Symbol keys and Pathname values.
+      def lookup_map
+        map = super
+
+        unless @has_filtered_lookup_map
+          @lookup_map.select! { |key, _| @data.key?(key) }
+          @has_filtered_lookup_map = true
+        end
+
+        map
+      end
 
       # Internal: Returns the static, exported values for the document
       # matching the given key.
@@ -36,7 +54,7 @@ module Atlas
       #
       # Returns a hash.
       def exported_data_for(key)
-        @data[key] || {}
+        @data[key]
       end
 
       # Internal: Given a document path or key, retrieves the attributes for
@@ -46,7 +64,7 @@ module Atlas
       # key  - The unique key which identifies the document.
       #
       # Returns a hash.
-      def load_attributes(path, key)
+      def load_attributes(_path, key)
         exported_data_for(key)
       end
     end
--- spec/atlas/production_mode_spec.rb
+++ spec/atlas/production_mode_spec.rb
@@ -1,19 +1,33 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 module Atlas
   describe ProductionMode do
-    let(:mode) { ProductionMode.new({ nodes: { foo: { demand: 50 } } }) }
-
-    it 'retrieves nodes' do
-      expect(mode.nodes.length).to eq(Atlas::Node.all.length)
+    let(:mode) do
+      described_class.new(
+        nodes: { foo: { demand: 50 } },
+        edges: {
+          'bar-fd@coal': { parent_share: 0.3 },
+          'foo-bar@coal': {}
+        }
+      )
     end
 
-    it 'uses static data' do
+    it 'retrieves nodes' do
+      expect(mode.nodes.length).to eq(1)
+    end
+
+    it 'uses static data for nodes' do
       expect(mode.nodes.find(:foo).demand).to eq(50.0)
     end
 
     it 'retrieves edges' do
-      expect(mode.edges.length).to eq(Atlas::Edge.all.length)
+      expect(mode.edges.length).to eq(2)
+    end
+
+    it 'uses static data for edges' do
+      expect(mode.edges.find(:'bar-fd@coal').parent_share).to eq(0.3)
     end
   end
 end
