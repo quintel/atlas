2188fdb85bab6a805e4515c3e4fe5aebbd808298
Anthony Williams <hi@antw.dev>
2021-04-21 15:49:45 +0100

    Sort methods in Runtime

--- lib/atlas/runtime.rb
+++ lib/atlas/runtime.rb
@@ -69,21 +69,30 @@ module Atlas
       dataset.public_send(property)
     end
 
-    # Public: Retrieves a share value identified by the given key.
+    # Public: Given the key of a node, retrieves the production (energy
+    # supplied) of the node from the central_producers.csv file.
     #
-    # file_key  - The name of the file in which to find the share value, minus
+    # node_key - The key of the node whose production is to be fetched.
+    #
+    # Returns a Float.
+    def CENTRAL_PRODUCTION(node_key, attribute = :demand)
+      dataset.central_producers.get(node_key, attribute)
+    end
+
+    # Public: Retrieves a demand value identified by the given key.
+    #
+    # file_key  - The name of the file in which to find the demand value, minus
     #             the ".csv" extension.
-    # attribute - The name of the attribute to be extracted from the share
+    # attribute - The name of the attribute to be extracted from the demand
     #             file.
     #
     # For example, retrieving the gasoline share from trucks.csv.
     #
-    #   SHARE(:trucks, :gasoline)
+    #   DEMAND(:industry, :final_demand_coal_gas)
     #
-    # Returns a Numeric, or raises NoSuchShareError if the file or attribute
-    # do not exist.
-    def SHARE(file_key, attribute)
-      dataset.shares(file_key).get(attribute)
+    # Returns a Numeric.
+    def DEMAND(file_key, attribute)
+      dataset.demands(file_key).get(attribute)
     end
 
     # Public: Retrieves an efficiency, typically assigned as a slot share
@@ -103,59 +112,6 @@ module Atlas
       direction == :output if direction == :out
 
       dataset.efficiencies(file_key).get("#{ direction }.#{ carrier }")
-    end
-
-    # Public: Given the key of a node, retrieves the production (energy
-    # supplied) of the node from the central_producers.csv file.
-    #
-    # node_key - The key of the node whose production is to be fetched.
-    #
-    # Returns a Float.
-    def CENTRAL_PRODUCTION(node_key, attribute = :demand)
-      dataset.central_producers.get(node_key, attribute)
-    end
-
-    # Public: Given the key of a node, retrieves the production (energy
-    # supplied) of the node from the primary_producers.csv file.
-    #
-    # node_key - The key of the node whose production is to be fetched.
-    #
-    # Returns a Float.
-    def PRIMARY_PRODUCTION(node_key, attribute)
-      dataset.primary_production.get(node_key, attribute)
-    end
-
-    def PARENT_VALUE(node_key, attribute)
-      dataset.parent_values.get(node_key, attribute)
-    end
-
-    # Public: Given a key from the time curves file, retrieves the associated
-    # value.
-    #
-    # Presently just a stub so that queries using it don't break. This will
-    # be implemented soon.
-    #
-    # key - The key identifying the row to be read.
-    #
-    # Returns a Float.
-    def TIME_CURVE(curve_key, attribute)
-      dataset.time_curve(curve_key).get(@dataset.analysis_year, attribute)
-    end
-
-    # Public: Retrieves a demand value identified by the given key.
-    #
-    # file_key  - The name of the file in which to find the demand value, minus
-    #             the ".csv" extension.
-    # attribute - The name of the attribute to be extracted from the demand
-    #             file.
-    #
-    # For example, retrieving the gasoline share from trucks.csv.
-    #
-    #   DEMAND(:industry, :final_demand_coal_gas)
-    #
-    # Returns a Numeric.
-    def DEMAND(file_key, attribute)
-      dataset.demands(file_key).get(attribute)
     end
 
     # Public: Retrieves a value from an FCE file.
@@ -184,6 +140,50 @@ module Atlas
     # or attribute does not exist.
     def FCE(file_key, carrier, attribute)
       dataset.fce(file_key).fetch(carrier.to_sym).fetch(attribute.to_sym)
+    end
+
+    def PARENT_VALUE(node_key, attribute)
+      dataset.parent_values.get(node_key, attribute)
+    end
+
+    # Public: Given the key of a node, retrieves the production (energy
+    # supplied) of the node from the primary_producers.csv file.
+    #
+    # node_key - The key of the node whose production is to be fetched.
+    #
+    # Returns a Float.
+    def PRIMARY_PRODUCTION(node_key, attribute)
+      dataset.primary_production.get(node_key, attribute)
+    end
+
+    # Public: Retrieves a share value identified by the given key.
+    #
+    # file_key  - The name of the file in which to find the share value, minus
+    #             the ".csv" extension.
+    # attribute - The name of the attribute to be extracted from the share
+    #             file.
+    #
+    # For example, retrieving the gasoline share from trucks.csv.
+    #
+    #   SHARE(:trucks, :gasoline)
+    #
+    # Returns a Numeric, or raises NoSuchShareError if the file or attribute
+    # do not exist.
+    def SHARE(file_key, attribute)
+      dataset.shares(file_key).get(attribute)
+    end
+
+    # Public: Given a key from the time curves file, retrieves the associated
+    # value.
+    #
+    # Presently just a stub so that queries using it don't break. This will
+    # be implemented soon.
+    #
+    # key - The key identifying the row to be read.
+    #
+    # Returns a Float.
+    def TIME_CURVE(curve_key, attribute)
+      dataset.time_curve(curve_key).get(@dataset.analysis_year, attribute)
     end
 
     private
